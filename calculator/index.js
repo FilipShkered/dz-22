b035b33beb88c78a5676318bf5c7e3c3c8eee7c0
Filip <example@gmail.com>
2023-04-22 10:01:37 +0300

    first commit

---
+++ calculator/index.js
@@ -0,0 +1,9 @@
+const { add } = require('./add')
+const { sub } = require('./sub.js')
+const { mult } = require('./mult')
+const { div } = require('./div')
+
+module.exports.add = add
+module.exports.sub = sub
+module.exports.mult = mult
+module.exports.div = div
